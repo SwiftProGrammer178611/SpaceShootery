cd614eecc88d1d8cb22f8dfcf5a2c887e4af7d4f
SwiftProGrammer178611 <165575838+SwiftProGrammer178611@users.noreply.github.com>
2026-07-16 00:21:14 -0400

    loaded ship into the game

--- index.js
+++ index.js
@@ -4,3 +4,50 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+class Player{
+    constructor() {
+        this.position = {
+            x:200,
+            y:200
+        }
+
+        this.velocity = {
+            x:0,
+            y:0
+        }
+
+        const image = new Image();
+        image.src = './spaceship.png'
+        image.onload = () => {
+            this.image = image
+            this.width = image.width
+            this.height = image.height
+        }
+    }
+
+    draw() {
+        if(this.image)
+            c.drawImage(
+                this.image,
+                this.position.x,
+                this.position.y,
+                this.width,
+                this.height
+            )
+        c.fillStyle = "red";
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+}
+
+const player = new Player()
+
+
+function animate() {
+    requestAnimationFrame(animate)
+        c.fillRect(0,0,canvas.width,canvas.height)
+        player.draw()
+    
+}
+
+animate()
